2075dbef421410fb21cc3fcdf8a146241c8fb7f9
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-11-07 20:23:51 +0300

    updated production code test passed

---
+++ calculator/app/src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        
+    }
+
+    Integer add(int num1,  int num2) {
+        return num1 + num2;
+    }
+}
