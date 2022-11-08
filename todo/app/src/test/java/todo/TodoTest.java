3b5a54cdd28e545e969962ae6a403aeb7514cddf
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-11-08 14:10:24 +0300

    add task testing -red-

--- todo/app/src/test/java/todo/TodoTest.java
+++ todo/app/src/test/java/todo/TodoTest.java
@@ -2,6 +2,8 @@ package todo;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class TodoTest {
@@ -9,5 +11,14 @@ public class TodoTest {
     public void shoulStartWithEmptyList() {
         Todo todo = new Todo();
         assertTrue("Initial task list is empty", todo.tasks.isEmpty() );
+    }
+
+    @Test
+    public void shouldAddTask() {
+        Todo todo = new Todo();
+        ArrayList<String> expectedList = new ArrayList<String>();
+        expectedList.add("Buy coffee");
+
+        assertEquals("Should reflect new task added", expectedList, todo.addTask("Buy Coffee"));
     }
 }
