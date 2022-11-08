dff078244205cd1de5db9ca7ddb916bdb1095132
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-11-08 14:04:28 +0300

    created Todo class -test passed-

---
+++ todo/app/src/main/java/todo/Todo.java
@@ -0,0 +1,10 @@
+package todo;
+
+import java.util.ArrayList;
+
+public class Todo {
+    ArrayList<String> tasks = new ArrayList<String>();
+    public static void main(String[] args) {
+        
+    }
+}
