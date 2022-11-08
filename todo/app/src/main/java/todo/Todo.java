871487273b707329b6178b9f086261e696df841a
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-11-08 14:23:08 +0300

    getTasks method tested -red-

--- todo/app/src/main/java/todo/Todo.java
+++ todo/app/src/main/java/todo/Todo.java
@@ -12,4 +12,6 @@ public class Todo {
         tasks.add(task);
         return tasks;
     }
+
+  
 }
--- todo/app/src/test/java/todo/TodoTest.java
+++ todo/app/src/test/java/todo/TodoTest.java
@@ -21,4 +21,19 @@ public class TodoTest {
 
         assertEquals("Should reflect new task added", expectedList, todo.addTask("Buy Coffee"));
     }
+
+    @Test
+    public void shouldGetTasks() {
+        Todo todo = new Todo();
+        ArrayList<String> expectedList = new ArrayList<String>();
+        expectedList.add("Buy Coffee");
+        expectedList.add("Buy Sugar");
+        expectedList.add("Buy Milk");
+
+        todo.addTask( "Buy Coffee");
+        todo.addTask( "Buy Sugar");
+        todo.addTask( "Buy Milk");
+
+        assertEquals("Should reflect all tasks stored", expectedList, todo.getTasks());
+    }
 }
